d99804fc083166dc0c7930408ea800ab6871ba98
amifsud02 <amifsud02@gmail.com>
2023-07-24 21:04:05 +0200

    Added Fallback to Main Functions

--- app/page.tsx
+++ app/page.tsx
@@ -17,28 +17,42 @@ import Footer from "@/components/Footer/Footer";
 
 
 const getFixtures = async () => {
-  const response = await fetch(
-    `http://localhost:3000/api/fixtures?competitiontypename=U21 Men's National League&season=2023&limit=5`
-  );
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/fixtures?competitiontypename=U21 Men's National League&season=2023&limit=5`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const fixtures = await response.json();
+    return fixtures;
+  } catch (error) {
+    console.error('Error fetching fixtures:', error);
+    // Fallback handling: Return a default value or an empty array.
+    return [];
   }
-  const fixtures = await response.json();
-  return fixtures;
 };
 
 const getStandings = async () => {
-  const response = await fetch(
-    `http://localhost:3000/api/standings?competitiontypename=U21%20Men%27s%20National%20League&season=2023`
-  );
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/standings?competitiontypename=U21%20Men%27s%20National%20League&season=2023`
+    );
 
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const standings = await response.json();
+    return standings;
+  } catch (error) {
+    console.error('Error fetching standings:', error);
+    // Fallback handling: Return a default value or an empty object.
+    return {};
   }
-
-  const standings = await response.json();
-  return standings
-}
+};
 
 export default async function Home() {
   const { fixtures }: { fixtures: IFixture[] } = await getFixtures();
@@ -55,48 +69,6 @@ export default async function Home() {
           <Tabs redirect="/season/current" showall={true}>
             <Tab tabTitle="Men">
               <Fixtures data={fixtures} showTitle={false}></Fixtures>
-              {/* <section className={styles.matchContainer}>
-                <div className={styles.matchWrapper}>
-                  {fixtures.map((fixture) => (
-                    <>
-                      <div className={styles.matchContent} key={fixture.awayTeamId.teamName}>
-                        <div className={styles.match}>
-                          <>
-                            <div className={styles.homeTeam}>
-                              <div className={styles.teamBadge}>
-
-                              </div>
-                              <span className={styles.teamName}>{fixture.homeTeamId.teamName}</span>
-                            </div>
-
-                            <div className={styles.matchDetails}>
-                              <div className={styles.matchType}>
-                                <h4>{fixture.status}</h4>
-                              </div>
-                              <div className={styles.matchScore}>
-                                <span>
-                                  {fixture.homeScore} - {fixture.awayScore}
-                                </span>
-                              </div>
-
-                              <div className={styles.matchDate}>
-                                04 June 2023<br />21:00
-                              </div>
-                            </div>
-
-                            <div className={styles.awayTeam}>
-                              <span className={styles.teamName}>{fixture.awayTeamId.teamName}</span>
-                              <div className={styles.teamBadge}>
-
-                              </div>
-                            </div>
-                          </>
-                        </div>
-                      </div>
-                    </>
-                  ))}
-                </div>
-              </section> */}
             </Tab>
 
             <Tab tabTitle="Women">
