407687d7fef14f7fcd6f683e52fd811b410aadc8
amifsud02 <amifsud02@gmail.com>
2023-11-14 19:25:55 +0100

    Bug/Fix - Added Ads

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,8 @@
 import PageHeader from "@/components/PageHeader/PageHeader";
 import ContactForm from "./contact-form";
 import Footer from "@/components/Footer/Footer";
-import { Partners }from "@/components/Partners/Partners";
+import { Partners } from "@/components/Partners/Partners";
+import AdSense from "@/components/AdSense/AdSense";
 
 const title = 'La Salle Handball | Contact Us'
 const description = 'Click here to reach out to our team regarding inquiries, job opportunities, future projects, or any other matters you find important.'
@@ -9,17 +10,17 @@ const baseSiteUrl = process.env.NEXT_PUBLIC_API_URL;
 const canonical = `${baseSiteUrl}/contact`;
 
 export const metadata = {
+  title: title,
+  description: description,
+  alternates: {
+    canonical: canonical
+  },
+  openGraph: {
     title: title,
     description: description,
-    alternates: {
-        canonical: canonical
-    },
-    openGraph: {
-        title: title,
-        description: description,
-        
-        url: canonical,
-    }
+
+    url: canonical,
+  }
 };
 
 export default function ContactPage() {
@@ -27,10 +28,13 @@ export default function ContactPage() {
     <>
       <PageHeader pageName="Contact Us" />
       <section className="parent">
+        <AdSense adSlot='4410526483'></AdSense>
+      </section>
+      <section className="parent">
         <ContactForm />
       </section>
-      <Partners/>
-      <Footer/>
+      <Partners />
+      <Footer />
     </>
   );
 }
--- components/AdSense/AdSense.tsx
+++ components/AdSense/AdSense.tsx
@@ -7,39 +7,52 @@ declare global {
     adsbygoogle: any[];
   }
 }
+// const AdSense = ({adSlot}: {adSlot: string}) => {
+//   useEffect(() => {
+//     const scriptElement = document.querySelector(
+//       'script[src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6327648024245847"]'
+//     )
+
+//     console.log(scriptElement);
+
+//     const handleScriptLoad = () => {
+//       try {
+//         if (window.adsbygoogle) {
+//           console.log('pushing ads');
+//           window.adsbygoogle.push({});
+//         } else {
+//           scriptElement!.addEventListener("load", handleScriptLoad);
+//           console.log("waiting until adsense lib is laoded");
+//         }
+//       } catch (err) {
+//         console.log("error in adsense", err)
+//       }
+//     }
+//   }, []);
+
+//   return (
+//     <ins
+//       className="adsbygoogle"
+//       style={{ display: "block" }}
+//       data-ad-client="ca-pub-6327648024245847" // Replace with your publisher ID
+//       data-ad-slot={adSlot}
+//       data-ad-format="auto"
+//       data-full-width-responsive="true"
+//     ></ins>
+//   );
+// };
+
+import { Adsense } from '@ctrl/react-adsense';
 const AdSense = ({adSlot}: {adSlot: string}) => {
-  useEffect(() => {
-    const scriptElement = document.querySelector(
-      'script[src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6327648024245847"]'
-    )
-
-    console.log(scriptElement);
-
-    const handleScriptLoad = () => {
-      try {
-        if (window.adsbygoogle) {
-          console.log('pushing ads');
-          window.adsbygoogle.push({});
-        } else {
-          scriptElement!.addEventListener("load", handleScriptLoad);
-          console.log("waiting until adsense lib is laoded");
-        }
-      } catch (err) {
-        console.log("error in adsense", err)
-      }
-    }
-  }, []);
-
   return (
-    <ins
-      className="adsbygoogle"
+    <Adsense
+      client="ca-pub-6327648024245847"
+      slot={adSlot}
       style={{ display: "block" }}
-      data-ad-client="ca-pub-6327648024245847" // Replace with your publisher ID
-      data-ad-slot={adSlot}
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    ></ins>
-  );
-};
+      layout="in-article"
+      format="fluid"
+    />
+  )
+}
 
 export default AdSense;
